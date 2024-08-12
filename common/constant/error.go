d9169370a99d5f8bde991b6eb029dfa9e2311875
zhanglp0129 <zhanglp0129@gmail.com>
2024-08-12 19:36:28 +0800

    完成登录接口

--- common/constant/error.go
+++ common/constant/error.go
@@ -4,8 +4,16 @@ const (
 	RedisPingError = "ping redis error"
 )
 
+// MailCheckCodeError 邮件验证码错误
 type MailCheckCodeError struct{}
 
-func (e MailCheckCodeError) Error() string {
+func (MailCheckCodeError) Error() string {
 	return "邮件验证码错误"
+}
+
+// UsernameEmailPasswordError 用户名、邮箱或密码错误
+type UsernameEmailPasswordError struct{}
+
+func (UsernameEmailPasswordError) Error() string {
+	return "用户名、邮箱或密码错误"
 }
--- service/user/internal/logic/registerlogin/userloginlogic.go
+++ service/user/internal/logic/registerlogin/userloginlogic.go
@@ -2,6 +2,11 @@ package registerloginlogic
 
 import (
 	"context"
+	"errors"
+	"gorm.io/gorm"
+	"zerooj/common/constant"
+	"zerooj/service/user/models"
+	"zerooj/utils"
 
 	"zerooj/service/user/internal/svc"
 	"zerooj/service/user/pb/user"
@@ -25,7 +30,29 @@ func NewUserLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserLog
 
 // 用户登录
 func (l *UserLoginLogic) UserLogin(in *user.UserLoginReq) (*user.UserLoginResp, error) {
-	// todo: add your logic here and delete this line
+	pwd, err := utils.PasswordEncrypt(in.Password)
+	if err != nil {
+		return nil, err
+	}
 
-	return &user.UserLoginResp{}, nil
+	db := l.svcCtx.DB
+	u := &models.User{}
+	if in.Username == "" {
+		// 邮箱登录
+		err = db.Where("email = ? and password = ?", in.Email, pwd).Take(u).Error
+	} else {
+		// 用户名登录
+		err = db.Where("username = ? and password = ?", in.Username, pwd).Take(u).Error
+	}
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		// 没有记录
+		return nil, constant.UsernameEmailPasswordError{}
+	} else if err != nil {
+		return nil, err
+	}
+
+	return &user.UserLoginResp{
+		Id:         u.ID,
+		Permission: uint32(u.Permission),
+	}, nil
 }
