b6bdace158c59f2763d5274079fe085bd20c65bd
zhanglp0129 <zhanglp0129@gmail.com>
2024-08-19 23:21:50 +0800

    完成忘记密码的接口

--- service/user/internal/logic/baseinfo/forgetpasswordlogic.go
+++ service/user/internal/logic/baseinfo/forgetpasswordlogic.go
@@ -2,6 +2,10 @@ package baseinfologic
 
 import (
 	"context"
+	"fmt"
+	"zerooj/common/constant"
+	"zerooj/service/user/models"
+	"zerooj/utils"
 
 	"zerooj/service/user/internal/svc"
 	"zerooj/service/user/pb/user"
@@ -23,9 +27,39 @@ func NewForgetPasswordLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Fo
 	}
 }
 
-// 忘记密码，需重新登录
+// 忘记密码
 func (l *ForgetPasswordLogic) ForgetPassword(in *user.ForgetPasswordReq) (*user.ForgetPasswordResp, error) {
-	// todo: add your logic here and delete this line
+	// 查询邮箱并验证邮箱验证码
+	db := l.svcCtx.DB
+	u := models.User{}
+	err := db.Select("email", "password").Take(&u, in.Id).Error
+	if err != nil {
+		return nil, err
+	}
+	rdb := l.svcCtx.RDB
+	key := fmt.Sprintf("/mail_check_code/user/forget_password/%s", u.Email)
+	trueCheckCode, err := rdb.Get(context.Background(), key).Result()
+	if err != nil {
+		return nil, err
+	}
+	if trueCheckCode != in.EmailCheckCode {
+		return nil, constant.InputDataError{Thing: "邮箱验证码"}
+	}
+
+	// 检查新旧密码是否相同
+	newPasswordEncrypt, err := utils.PasswordEncrypt(in.NewPassword)
+	if err != nil {
+		return nil, err
+	}
+	if newPasswordEncrypt == u.Password {
+		return nil, constant.NewEqualsOldError{Thing: "密码"}
+	}
+
+	// 修改密码
+	err = db.Model(&u).Where("id = ?", in.Id).Update("password", newPasswordEncrypt).Error
+	if err != nil {
+		return nil, err
+	}
 
 	return &user.ForgetPasswordResp{}, nil
 }
