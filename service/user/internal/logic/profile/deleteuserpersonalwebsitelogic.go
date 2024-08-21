899873364c0fe2625286562e5811bd553c6e5a7c
zhanglp0129 <zhanglp0129@gmail.com>
2024-08-21 17:14:36 +0800

    完成了删除用户个人网站的接口

--- service/user/internal/logic/profile/deleteuserpersonalwebsitelogic.go
+++ service/user/internal/logic/profile/deleteuserpersonalwebsitelogic.go
@@ -2,6 +2,11 @@ package profilelogic
 
 import (
 	"context"
+	"fmt"
+	"github.com/zhanglp0129/redis_cache"
+	"gorm.io/gorm"
+	"zerooj/common/constant"
+	"zerooj/service/user/models"
 
 	"zerooj/service/user/internal/svc"
 	"zerooj/service/user/pb/user"
@@ -25,7 +30,31 @@ func NewDeleteUserPersonalWebsiteLogic(ctx context.Context, svcCtx *svc.ServiceC
 
 // 删除个人网站
 func (l *DeleteUserPersonalWebsiteLogic) DeleteUserPersonalWebsite(in *user.DeleteUserPersonalWebsiteReq) (*user.DeleteUserPersonalWebsiteResp, error) {
-	// todo: add your logic here and delete this line
+	// 校验该网站是否属于该用户
+	db := l.svcCtx.DB
+	w := models.PersonalWebsite{}
+	err := db.Select("user_id").Where("id = ?", in.WebsiteId).Take(&w).Error
+	if err != nil {
+		return nil, err
+	} else if w.UserID != in.UserId {
+		return nil, constant.InsufficientPermissionsError{}
+	}
+
+	err = db.Transaction(func(tx *gorm.DB) error {
+		err := tx.Delete(&models.PersonalWebsite{}, in.WebsiteId).Error
+		if err != nil {
+			return err
+		}
+
+		// 删除缓存
+		rdb := l.svcCtx.RDB
+		key := fmt.Sprintf("/cache/user/get_user_profile/%d", in.UserId)
+		err = redis_cache.DeleteCache(rdb, key)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
 
 	return &user.DeleteUserPersonalWebsiteResp{}, nil
 }
