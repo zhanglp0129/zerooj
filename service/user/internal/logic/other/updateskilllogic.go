755db865d8e4ed3e7d510f02a170fd99d40113a8
zhanglp0129 <zhanglp0129@gmail.com>
2024-08-23 22:19:31 +0800

    完成了修改技能的接口

--- service/user/internal/logic/other/updateskilllogic.go
+++ service/user/internal/logic/other/updateskilllogic.go
@@ -2,6 +2,10 @@ package otherlogic
 
 import (
 	"context"
+	"zerooj/common/constant"
+	common_models "zerooj/common/models"
+	baseinfologic "zerooj/service/user/internal/logic/baseinfo"
+	"zerooj/service/user/models"
 
 	"zerooj/service/user/internal/svc"
 	"zerooj/service/user/pb/user"
@@ -25,7 +29,19 @@ func NewUpdateSkillLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Updat
 
 // 修改技能，需要客服权限
 func (l *UpdateSkillLogic) UpdateSkill(in *user.UpdateSkillReq) (*user.UpdateSkillResp, error) {
-	// todo: add your logic here and delete this line
+	// 鉴权
+	baseInfo, err := baseinfologic.GetBaseInfo(l.svcCtx, in.OperatorId)
+	if err != nil {
+		return nil, err
+	}
+	if !common_models.Permission(baseInfo.Permission).CanSupport() {
+		return nil, constant.InsufficientPermissionsError{}
+	}
 
+	db := l.svcCtx.DB
+	err = db.Model(&models.Skill{}).Where("id = ?", in.SkillId).Update("name", in.SkillName).Error
+	if err != nil {
+		return nil, err
+	}
 	return &user.UpdateSkillResp{}, nil
 }
