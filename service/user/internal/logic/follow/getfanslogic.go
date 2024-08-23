f550cd7bce23a293d94bc77ff8ea96a0634839f0
zhanglp0129 <zhanglp0129@gmail.com>
2024-08-23 15:01:38 +0800

    完成了获取粉丝的接口

--- service/user/internal/logic/follow/getfanslogic.go
+++ service/user/internal/logic/follow/getfanslogic.go
@@ -2,6 +2,12 @@ package followlogic
 
 import (
 	"context"
+	"fmt"
+	"github.com/zhanglp0129/redis_cache"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	baseinfologic "zerooj/service/user/internal/logic/baseinfo"
+	profilelogic "zerooj/service/user/internal/logic/profile"
+	"zerooj/service/user/models"
 
 	"zerooj/service/user/internal/svc"
 	"zerooj/service/user/pb/user"
@@ -25,7 +31,54 @@ func NewGetFansLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetFansLo
 
 // 获取所有粉丝，分页查询
 func (l *GetFansLogic) GetFans(in *user.GetFansReq) (*user.GetFansResp, error) {
-	// todo: add your logic here and delete this line
+	db := l.svcCtx.DB
+	rdb := l.svcCtx.RDB
+	var count int64
+	key := fmt.Sprintf("/cache/user/get_fans/count/%d", in.UserId)
+	// 获取总数
+	_, err := redis_cache.QueryWithCache(rdb, key, &count, func() (*int64, error) {
+		var total int64
+		err := db.Model(&models.Follow{}).Where("followed_id = ?", in.UserId).Count(&total).Error
+		if err != nil {
+			return nil, err
+		}
+		return &total, nil
+	})
+	if err != nil {
+		return nil, err
+	}
 
-	return &user.GetFansResp{}, nil
+	// 分页查询
+	var offset = (in.PageNum - 1) * in.PageSize
+	follows := make([]models.Follow, 0)
+	err = db.Where("followed_id = ?", in.UserId).Offset(int(offset)).Limit(int(in.PageSize)).Find(&follows).Error
+	if err != nil {
+		return nil, err
+	}
+	fans := make([]*user.FollowUserInfo, 0, len(follows))
+	for _, follow := range follows {
+		baseInfo, err := baseinfologic.GetBaseInfo(l.svcCtx, follow.FollowerID)
+		if err != nil {
+			return nil, err
+		}
+		profile, err := profilelogic.GetUserProfile(l.svcCtx, follow.FollowerID)
+		if err != nil {
+			return nil, err
+		}
+		fans = append(fans, &user.FollowUserInfo{
+			Id:                   follow.FollowedID,
+			Username:             baseInfo.Username,
+			Nickname:             profile.Nickname,
+			PersonalIntroduction: profile.PersonalIntroduction,
+			AvatarURL:            profile.AvatarURL,
+			Websites:             profile.Websites,
+			Skills:               profile.Skills,
+			FollowAt:             timestamppb.New(follow.FollowAt),
+		})
+	}
+
+	return &user.GetFansResp{
+		Count: count,
+		Fans:  fans,
+	}, nil
 }
