af1d75c12eb8c84d9a396ad975d3693d451c2764
zhanglp0129 <zhanglp0129@gmail.com>
2024-09-10 15:12:02 +0800

    语言不再区分版本

--- service/problemset/models/language.go
+++ service/problemset/models/language.go
@@ -7,8 +7,7 @@ import (
 
 type Language struct {
 	models.Model
-	Name    string `gorm:"size:20;comment:语言名称"`
-	Version string `gorm:"size:20;comment:语言版本"`
+	Name string `gorm:"size:20;comment:语言名称"`
 }
 
 type ProblemLanguage struct {
